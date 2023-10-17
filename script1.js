ef1033c9c282524bba85fe70ba0f66618869363b
[Jainish] <[jeinishgamit@gmail.com]>
2023-10-17 13:10:57 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,14 @@
+//Q1: Write a javascript functin to check whether an 'input' is an array or not
+
+//1)
+//  function checkArr(inpt){
+//     return Array.isArray(inpt);
+
+// }
+// console.log(checkArr([2]));
+
+//2)
+//   function checkArr(inp){
+//     return inp instanceof Array;
+//   }
+//   console.log(checkArr([1,2,3,4]));
